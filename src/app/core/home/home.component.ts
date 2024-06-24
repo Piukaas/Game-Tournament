4a04ee88099c0190bd53a75ae238faf7085b0bac
Thijs <thijsvanrijsbergen@gmail.com>
2024-06-24 18:12:47 +0200

    Refactor home component for improved layout and data fetching

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy } from '@angular/core';
+import { Subscription, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { UserService } from '../../services/user.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'home',
@@ -9,18 +11,70 @@ import { Subscription } from 'rxjs';
 export class HomeComponent implements OnDestroy {
   isLoggedIn: boolean = false;
   private authSub: Subscription | undefined;
+  loading: boolean = false;
+  tournament: any;
+  games: any;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.authSub = this.userService.authStatus$.subscribe((isLoggedIn) => {
       this.isLoggedIn = isLoggedIn;
     });
+
+    this.getMostRecentTournament();
+    this.getMostRecentGames();
   }
 
   ngOnDestroy(): void {
     if (this.authSub) {
       this.authSub.unsubscribe();
+    }
+  }
+
+  getMostRecentTournament() {
+    this.loading = true;
+    this.http.get(`${environment.apiUrl}/tournaments/recent`).subscribe(
+      (response: any) => {
+        this.tournament = response;
+        console.log(this.tournament);
+        this.loading = false;
+      },
+      (error) => {
+        this.loading = false;
+        throw of(error);
+      }
+    );
+  }
+
+  getMostRecentGames() {
+    this.loading = true;
+    const numberOfGames = window.innerWidth <= 768 ? 3 : 6;
+
+    this.http
+      .get(`${environment.apiUrl}/games/random/${numberOfGames}`)
+      .subscribe(
+        (response: any) => {
+          this.games = response;
+          this.loading = false;
+        },
+        (error) => {
+          this.loading = false;
+          throw of(error);
+        }
+      );
+  }
+
+  getBadgeClass(status: string): string {
+    switch (status) {
+      case 'Aankomend':
+        return 'badge bg-primary';
+      case 'Afgerond':
+        return 'badge bg-danger';
+      case 'Actief':
+        return 'badge bg-success';
+      default:
+        return 'badge';
     }
   }
 }
